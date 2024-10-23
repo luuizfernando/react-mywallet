9b66f823e9217c12092962f8025990dd054933fe
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-23 12:07:13 -0300

    Tela SignUp feita

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,44 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from 'axios';
 
 export default function SignUpPage() {
+  const [form, setForm] = useState({ name: "", email: "", password: "", confirmPassword: "" });
+
+  const navigate = useNavigate();
+
+  function handleForm(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    if (form.password !== form.confirmPassword) return alert("As senhas não coincidem");
+
+    delete form.confirmPassword;
+    axios.post(`${process.env.REACT_APP_API_URL}/sign-up`, form)
+      .then(res => {
+        alert("Usuário criado com sucesso!");
+        navigate("/");
+      })
+      .catch(err => console.log(err.response.data));
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={submitForm}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input required placeholder="Nome" name="name" value={form.name} onChange={handleForm} />
+        <input required type="email" autoComplete="username" placeholder="E-mail" name="email" value={form.email} onChange={handleForm} />
+        <input required minLength={3} type="password" autoComplete="new-password" placeholder="Senha" name="password" value={form.password} onChange={handleForm} />
+        <input required minLength={3} type="password" autoComplete="new-password" placeholder="Confirme a senha" name="confirmPassword" value={form.confirmPassword} onChange={handleForm} />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to={"/"}>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
