56d8c36de4147a1a1bde96859d072f8024ba22f5
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-29 11:57:59 -0300

    Correcao rotas app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
-import HomePage from "./pages/HomePage";
-import SignInPage from "./pages/SignInPage";
-import SignUpPage from "./pages/SignUpPage";
-import AuthContext from "./contexts/AuthContext";
-import { useState } from "react";
-import AddTransactionsPage from "./pages/AddTransactionPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import styled from "styled-components"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+import AddTransactionsPage from "./pages/AddTransactionPage"
+import EditTransactionsPage from "./pages/EditTransactionPage"
+import AuthContext from "./contexts/AuthContext"
+import { useState } from "react"
+import { mainColor } from "./constants/colors"
 
 export default function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [userName, setUserName] = useState(localStorage.getItem("userName"));
+  const [token, setToken] = useState(localStorage.getItem("token"))
+  const [userName, setUserName] = useState(localStorage.getItem("userName"))
 
   return (
     <PagesContainer>
       <AuthContext.Provider value={{ token, setToken, userName, setUserName }}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<SignInPage />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/cadastro" element={<SignUpPage />} />
             <Route path="/home" element={<HomePage />} />
-            <Route path="/nova-transacao/:tipo" element={<AddTransactionsPage />} />
+            <Route path="/nova-transacao/:type" element={<AddTransactionsPage />} />
           </Routes>
         </BrowserRouter>
       </AuthContext.Provider>
@@ -28,7 +30,7 @@ export default function App() {
 }
 
 const PagesContainer = styled.main`
-  background-color: #8c11be;
+  background-color:  ${mainColor};
   width: calc(100vw - 50px);
   max-height: 100vh;
   padding: 25px;
